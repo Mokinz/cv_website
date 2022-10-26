4c3f8c79a412ce1cd0b67b07ba8b446752030534
daniel.drapala <daniel.drapala@ocado.com>
2022-10-26 15:36:04 +0200

    clear code

--- scripts/spinImageOnClick.js
+++
@@ -1,9 +0,0 @@
-$(function() {
-    $('body')
-      .on("click", 'img', function(){
-      $(this).addClass('animate');
-    })
-      .on("animationend", 'img', function(){
-      $(this).removeClass('animate');
-    });
-	});
